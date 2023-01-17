124cd1238854b24be7d59929cf4e0c501e3b1efa
Inspayaa <tobechukwujoel@gmail.com>
2023-01-17 03:11:49 +0100

    Update CalcFunction.cs

--- Assets/Script/CalcFunction.cs
+++ Assets/Script/CalcFunction.cs
@@ -6,10 +6,11 @@ public class CalcFunction : MonoBehaviour
 {
     private int ops;
 
-    private float num1;
-    private float num2;
-    public float answer;
+    private double num1;
+    private double num2;
+    public double answer;
 
+    private bool clearOperator;
     private bool isOperatorActive;
     private bool isDecimalAlready;
 
@@ -20,6 +21,7 @@ public class CalcFunction : MonoBehaviour
     void Start()
     {
         firstDisplay.text = "0";
+        secondDisplay.text = "";
         isDecimalAlready = false;
     }
 
@@ -45,12 +47,12 @@ public class CalcFunction : MonoBehaviour
     public void One()
     {
        
-        if (firstDisplay.text.Length <= 10)
+        if (firstDisplay.text.Length >= 1 && firstDisplay.text.Length <= 8)
         {
-            if (firstDisplay.text.Length == 1)
+            if (firstDisplay.text.Length == 1 && firstDisplay.text == "0" || !clearOperator)
             {
                 firstDisplay.text = "";
-
+                clearOperator = true;
             }
 
             UpdateDisplayText("1");
@@ -61,23 +63,24 @@ public class CalcFunction : MonoBehaviour
 
         if (!isOperatorActive) 
         {
-            num1 = float.Parse(firstDisplay.text);
+            num1 = double.Parse(firstDisplay.text);
         }
 
         else
         {
-            num2 = float.Parse(firstDisplay.text);
+            num2 = double.Parse(firstDisplay.text);
         }
         
     }
 
     public void Two()
     {
-        if (firstDisplay.text.Length >= 1 && firstDisplay.text.Length <= 10)
+        if (firstDisplay.text.Length >= 1 && firstDisplay.text.Length <= 8)
         {
-            if (firstDisplay.text.Length == 1)
+            if (firstDisplay.text.Length == 1 && firstDisplay.text == "0" || !clearOperator)
             {
                 firstDisplay.text = "";
+                clearOperator = true;
             }
 
             UpdateDisplayText("2");
@@ -88,22 +91,23 @@ public class CalcFunction : MonoBehaviour
 
         if (!isOperatorActive)
         {
-            num1 = float.Parse(firstDisplay.text);
+            num1 = double.Parse(firstDisplay.text);
         }
 
         else
         {
-            num2 = float.Parse(firstDisplay.text);
+            num2 = double.Parse(firstDisplay.text);
         }
     }
 
     public void Three()
     {
-        if (firstDisplay.text.Length >= 1 && firstDisplay.text.Length <= 10)
+        if (firstDisplay.text.Length >= 1 && firstDisplay.text.Length <= 8)
         {
-            if (firstDisplay.text.Length == 1)
+            if (firstDisplay.text.Length == 1 && firstDisplay.text == "0" || !clearOperator)
             {
-                firstDisplay.text = "";
+                firstDisplay.text = ""; 
+                clearOperator = true;
             }
 
             UpdateDisplayText("3");
@@ -114,22 +118,23 @@ public class CalcFunction : MonoBehaviour
 
         if (!isOperatorActive)
         {
-            num1 = float.Parse(firstDisplay.text);
+            num1 = double.Parse(firstDisplay.text);
         }
 
         else
         {
-            num2 = float.Parse(firstDisplay.text);
+            num2 = double.Parse(firstDisplay.text);
         }
     }
 
     public void Four()
     {
-        if (firstDisplay.text.Length >= 1 && firstDisplay.text.Length <= 10)
+        if (firstDisplay.text.Length >= 1 && firstDisplay.text.Length <= 8)
         {
-            if (firstDisplay.text.Length == 1)
+            if (firstDisplay.text.Length == 1 && firstDisplay.text == "0" || !clearOperator)
             {
                 firstDisplay.text = "";
+                clearOperator = true;
             }
 
             UpdateDisplayText("4");
@@ -140,22 +145,23 @@ public class CalcFunction : MonoBehaviour
 
         if (!isOperatorActive)
         {
-            num1 = float.Parse(firstDisplay.text);
+            num1 = double.Parse(firstDisplay.text);
         }
 
         else
         {
-            num2 = float.Parse(firstDisplay.text);
+            num2 = double.Parse(firstDisplay.text);
         }
     }
 
     public void Five()
     {
-        if (firstDisplay.text.Length >= 1 && firstDisplay.text.Length <= 10)
+        if (firstDisplay.text.Length >= 1 && firstDisplay.text.Length <= 8)
         {
-            if (firstDisplay.text.Length == 1)
+            if (firstDisplay.text.Length == 1 && firstDisplay.text == "0" || !clearOperator )
             {
                 firstDisplay.text = "";
+                clearOperator = true;
             }
 
             UpdateDisplayText("5");
@@ -166,22 +172,23 @@ public class CalcFunction : MonoBehaviour
 
         if (!isOperatorActive)
         {
-            num1 = float.Parse(firstDisplay.text);
+            num1 = double.Parse(firstDisplay.text);
         }
 
         else
         {
-            num2 = float.Parse(firstDisplay.text);
+            num2 = double.Parse(firstDisplay.text);
         }
     }
 
     public void Six()
     {
-        if (firstDisplay.text.Length >= 1 && firstDisplay.text.Length <= 10)
+        if (firstDisplay.text.Length >= 1 && firstDisplay.text.Length <= 8)
         {
-            if (firstDisplay.text.Length == 1)
+            if (firstDisplay.text.Length == 1 && firstDisplay.text == "0" || !clearOperator)
             {
                 firstDisplay.text = "";
+                clearOperator = true;
             }
 
             UpdateDisplayText("6");
@@ -192,22 +199,23 @@ public class CalcFunction : MonoBehaviour
 
         if (!isOperatorActive)
         {
-            num1 = float.Parse(firstDisplay.text);
+            num1 = double.Parse(firstDisplay.text);
         }
 
         else
         {
-            num2 = float.Parse(firstDisplay.text);
+            num2 = double.Parse(firstDisplay.text);
         }
     }
 
     public void Seven()
     {
-        if (firstDisplay.text.Length >= 1 && firstDisplay.text.Length <= 10)
+        if (firstDisplay.text.Length >= 1 && firstDisplay.text.Length <= 8)
         {
-            if (firstDisplay.text.Length == 1)
+            if (firstDisplay.text.Length == 1 && firstDisplay.text == "0" || !clearOperator)
             {
                 firstDisplay.text = "";
+                clearOperator = true;
             }
 
             UpdateDisplayText("7");
@@ -218,22 +226,23 @@ public class CalcFunction : MonoBehaviour
 
         if (!isOperatorActive)
         {
-            num1 = float.Parse(firstDisplay.text);
+            num1 = double.Parse(firstDisplay.text);
         }
 
         else
         {
-            num2 = float.Parse(firstDisplay.text);
+            num2 = double.Parse(firstDisplay.text);
         }
     }
 
     public void Eight()
     {
-        if (firstDisplay.text.Length >= 1 && firstDisplay.text.Length <= 10)
+        if (firstDisplay.text.Length >= 1 && firstDisplay.text.Length <= 8)
         {
-            if (firstDisplay.text.Length == 1)
+            if (firstDisplay.text.Length == 1 && firstDisplay.text == "0" || !clearOperator)
             {
                 firstDisplay.text = "";
+                clearOperator = true;
             }
 
             UpdateDisplayText("8");
@@ -244,22 +253,23 @@ public class CalcFunction : MonoBehaviour
 
         if (!isOperatorActive)
         {
-            num1 = float.Parse(firstDisplay.text);
+            num1 = double.Parse(firstDisplay.text);
         }
 
         else
         {
-            num2 = float.Parse(firstDisplay.text);
+            num2 = double.Parse(firstDisplay.text);
         }
     }
 
     public void Nine()
     {
-        if (firstDisplay.text.Length >= 1 && firstDisplay.text.Length <= 10)
+        if (firstDisplay.text.Length >= 1 && firstDisplay.text.Length <= 8)
         {
-            if (firstDisplay.text.Length == 1)
+            if (firstDisplay.text.Length == 1 && firstDisplay.text == "0" || !clearOperator)
             {
                 firstDisplay.text = "";
+                clearOperator = true;
             }
 
             UpdateDisplayText("9");
@@ -270,22 +280,23 @@ public class CalcFunction : MonoBehaviour
 
         if (!isOperatorActive)
         {
-            num1 = float.Parse(firstDisplay.text);
+            num1 = double.Parse(firstDisplay.text);
         }
 
         else
         {
-            num2 = float.Parse(firstDisplay.text);
+            num2 = double.Parse(firstDisplay.text);
         }
     }
 
     public void Zero()
     {
-        if (firstDisplay.text.Length >= 1 && firstDisplay.text.Length <= 10)
+        if (firstDisplay.text.Length >= 1 && firstDisplay.text.Length <= 8)
         {
-            if (firstDisplay.text.Length == 1)
+            if (firstDisplay.text.Length == 1 && firstDisplay.text == "0" || !clearOperator)
             {
                 firstDisplay.text = "";
+                clearOperator = true;
             }
 
             UpdateDisplayText("0");
@@ -296,12 +307,12 @@ public class CalcFunction : MonoBehaviour
 
         if (!isOperatorActive)
         {
-            num1 = float.Parse(firstDisplay.text);
+            num1 = double.Parse(firstDisplay.text);
         }
 
         else
         {
-            num2 = float.Parse(firstDisplay.text);
+            num2 = double.Parse(firstDisplay.text);
         }
     }
 
@@ -319,6 +330,7 @@ public class CalcFunction : MonoBehaviour
     public void Multiplication()
     {
         isOperatorActive = true;
+        clearOperator = false;
         firstDisplay.text = "*";
         ops = 0;
     }
@@ -326,6 +338,7 @@ public class CalcFunction : MonoBehaviour
     public void Division()
     {
         isOperatorActive = true;
+        clearOperator = false;
         firstDisplay.text = "/";
         ops = 1;
     }
@@ -333,6 +346,7 @@ public class CalcFunction : MonoBehaviour
     public void Addition()
     {
         isOperatorActive = true;
+        clearOperator = false;
         firstDisplay.text = "+";
         ops = 2;
     }
@@ -340,6 +354,7 @@ public class CalcFunction : MonoBehaviour
     public void Substraction()
     {
         isOperatorActive = true;
+        clearOperator = false;
         firstDisplay.text = "-";
         ops = 3;
     }
@@ -347,6 +362,7 @@ public class CalcFunction : MonoBehaviour
     public void Percentage()
     {
         isOperatorActive = true;
+        clearOperator = false;
         firstDisplay.text = "%";
         ops = 4;
     }
@@ -377,19 +393,19 @@ public class CalcFunction : MonoBehaviour
         switch (operation)
         {
             case 0:
-                answer = num1 * float.Parse(firstDisplay.text);
+                answer = num1 * num2;
                 break;
 
             case 1:
-                answer = num1 / float.Parse(firstDisplay.text);
+                answer = num1 / num2;
                 break;
 
             case 2:
-                answer = num1 + float.Parse(firstDisplay.text);
+                answer = num1 + num2;
                 break;
 
             case 3:
-                answer = num1 - float.Parse(firstDisplay.text);
+                answer = num1 - num2;
                 break;
 
             case 4:
